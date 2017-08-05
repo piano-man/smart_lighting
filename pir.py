1a2f0a232df949cb2a8cc69750f4bb3a7e6f1ccb
piano-man <icm2015003@iiita.ac.in>
2017-08-05 10:47:28 +0000

    infrared sensor code added

---
+++ pir.py
@@ -0,0 +1,19 @@
+import RPi.GPIO as GPIO #Import GPIO library
+import time #Import time library 
+
+GPIO.setmode(GPIO.BOARD) #Set GPIO pin numbering 
+
+pir = 11 #Associate pin 26 to pir 
+
+GPIO.setup(pir, GPIO.IN) #Set pin as GPIO in print "Waiting for sensor to settle" 
+
+time.sleep(2) #Waiting 2 seconds for the sensor to initiate print "Detecting motion" 
+
+while True: 
+
+	if GPIO.input(pir): #Check whether pir is HIGH print "Motion Detected!"
+		print("Motion Detected") 
+
+		time.sleep(2) #D1- Delay to avoid multiple detection
+
+	time.sleep(0.1) #While loop delay should be less than detection(hardware) delay
