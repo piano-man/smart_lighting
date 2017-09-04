61d04304abc0cebe004e1853331483430f9ed307
piano-man <icm2015003@iiita.ac.in>
2017-09-04 23:18:39 +0530

    config variables set

--- web_interface/app.py
+++ web_interface/app.py
@@ -1,23 +1,30 @@
-from flask import Flask, request
+from flask import Flask, request,render_template
 from flask_pymongo import PyMongo
 
 app = Flask(__name__)
 
 app.config['MONGO_DBNAME'] = 'lightdb'
-app.config['MONGO_URI'] = 'mongodb://username:password@database_id:port'
+app.config['MONGO_HOST'] = 'localhost'
+app.config['MONGO_PORT'] = 27017
+
+#app.config['MONGO_URI'] = 'mongodb://username:password@database_id:port'
 
 mongo = PyMongo(app)
 
 @app.route('/')
 def index():
+    print("hello")
     return 'Welcome to Smart Lighting Web Interface!'
 
 @app.route('/intensity', methods=['GET', 'POST'])
 def intensity():
     light_data = request.get_json()
-    light =  mongo.db.light
+    #print(light_data['value'])
+    #return render_template('index.html')
+#    print("hello")
+    light =  mongo.db.light_intensity
     light.insert(light_data)
     return 'Intensity Data : %s %s %s' % (light_data['pi_token'], str(light_data['module_no']), str(light_data['intensity']))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0')
